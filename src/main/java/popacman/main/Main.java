8bf5e60dd2684f1fabf575aac5c6ec57d5d73391
Jacob Schrum <schrum2@southwestern.edu>
2018-10-26 13:11:23 -0500

    Run with starter ghosts instead of random
    These are the ghosts that were evolved against, and also the ghosts that we are trying to perform well against in MsPacManEntry. Note that the performance of this agent in MM-NEAT is clearly visibly better than what we have been seeing in MsPacManEntry.

--- src/main/java/popacman/main/Main.java
+++ src/main/java/popacman/main/Main.java
@@ -3,13 +3,11 @@ package popacman.main;
 
 import java.util.EnumMap;
 
+import edu.southwestern.tasks.popacman.controllers.MyPacMan;
 import pacman.Executor;
 import pacman.controllers.IndividualGhostController;
 import pacman.controllers.MASController;
-import edu.southwestern.tasks.popacman.controllers.MyPacMan;
 import pacman.game.Constants.GHOST;
-import popacman.entrants.ghosts.spooky.*;
-import popacman.entrants.pacman.spooky.*;
 
 /**
  * Created by pwillic on 06/05/2016.
@@ -19,17 +17,19 @@ public class Main {
     public static void main(String[] args) {
 
         Executor executor = new Executor.Builder()
-                .setVisual(true)
-                .setTickLimit(4000)
-                .build();
+        		.setVisual(true)
+        	  	.setTickLimit(4000)
+        	  	.build();		
+        
+    	EnumMap<GHOST, IndividualGhostController> controllers = new EnumMap<>(GHOST.class);
 
-        EnumMap<GHOST, IndividualGhostController> controllers = new EnumMap<>(GHOST.class);
-
-        controllers.put(GHOST.INKY, new George());
-        controllers.put(GHOST.BLINKY, new Ringo());
-        controllers.put(GHOST.PINKY, new Paul());
-        controllers.put(GHOST.SUE, new John());
-
-        executor.runGameTimed(new MyPacMan(), new MASController(controllers));
+        MyPacMan badboy = new MyPacMan();
+        
+        controllers.put(GHOST.INKY, new popacman.examples.StarterGhost.POGhost(GHOST.INKY));
+        controllers.put(GHOST.BLINKY, new popacman.examples.StarterGhost.POGhost(GHOST.BLINKY));
+        controllers.put(GHOST.PINKY, new popacman.examples.StarterGhost.POGhost(GHOST.PINKY));
+        controllers.put(GHOST.SUE, new popacman.examples.StarterGhost.POGhost(GHOST.SUE));
+        
+        executor.runGameTimed(badboy, new MASController(controllers));
     }
 }
